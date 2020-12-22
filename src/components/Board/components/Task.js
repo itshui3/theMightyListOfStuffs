2dbb20d780dd8f8f952736ce7bd4c63209c819c2
Hui <foo@foo.com>
2020-12-22 12:43:57 -0800

    trying remove todo.js

--- src/components/Board/components/Task.js
+++ src/components/Board/components/Task.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 
 import './Task.css'
 
+
 function Task({ task, taskID }) {
 
     const [addingTodo, setAddingTodo] = useState(false)
@@ -14,39 +15,44 @@ function Task({ task, taskID }) {
         setAddingTodo(false)
     }
 
+
+
 return (
 <>
 
 <div className='task_card' key={taskID} id={taskID}>
     <h3 className='task_title'>{ task.name }</h3>
     {
-    task.todos.length > 0
-        ?
-        task.todos.map((todo, todoID) => (
-        <>
+        task.todos.length > 0
+            ?
+            task.todos.map((todo, todoID) => (
+            <>
             <div className='todo_card' key={todoID}>
             { todo }
             </div>
-            {
-                addingTodo
-                ?
-                <input 
-                className='todo_card' 
-                placeholder='Add Todo'
-                onBlur={stopAddingTodo}
-                />
-                :
-                <button 
-                className='todo_card' 
-                placeholder='Add Todo'
-                onClick={startAddingTodo}
-                />
-            }
 
-        </>
-        ))
+            </>
+            ))
+            :
+            null
+    }
+
+    {
+
+        addingTodo
+        ?
+        <input 
+        className='todo_card' 
+        placeholder='Add Todo'
+        onBlur={stopAddingTodo}
+        />
         :
-        null
+        <button 
+        className='todo_card' 
+        placeholder='Add Todo'
+        onClick={startAddingTodo}
+        />
+
     }
 </div>
 

4dc8c3ce95065bbfb4736ed2baac5cee1f0ea77d
Hui <foo@foo.com>
2020-12-22 11:06:30 -0800

    builds a dummylist with test data

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -3,19 +3,31 @@ import './Board.css'
 
 import { useState } from 'react'
 
+import { dummyList } from './assets/dummyList.js'
+
 function Board() {
 
     const [boardTitle, setBoardTitle] = useState(null)
-    
+    const [list, setList] = useState(() => {
+        return dummyList
+    })
+
     return (
         <>
         <div className='board_wrapper'>
+
             <div className='board_header'>
                 <h1 className='board_title header_items'>
                     {
-                    boardTitle ? boardTitle : 'Placeholder'
+                    boardTitle 
+                        ? boardTitle 
+                        : 'Placeholder'
                     }
                 </h1>
+            </div>
+
+            <div className='board_body'>
+
             </div>
                 
         </div>
---
+++ src/components/Board/assets/dummyList.js
@@ -0,0 +1,6 @@
+const dummylist = [
+    { name: 'Clean', description: 'Take out trash etc' },
+    { name: 'Code', description: 'Write da codes'}
+]
+
+export { dummylist }

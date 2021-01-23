6b7d642f5f6f6e4c39dab3ef21f3f8054f91af05
Hui <xuhuizhu369@gmail.com>
2021-01-22 21:34:05 -0800

    adds root board inputwrapper with boardInput child conditionally rendering

--- src/components/Dashboard/Input/BoardInput.js
+++ src/components/Dashboard/Input/BoardInput.js
@@ -1,47 +1,41 @@
 import './_BoardInput.css'
 import React, { useState, useEffect, useRef } from 'react'
 
-function BoardInput({ pushBoard }) {
+function BoardInput({ handleSave, unMountOnBlur }) {
 
     const [board, setBoard] = useState('')
-    const [addingBoard, setAddingBoard] = useState(false)
+
     const [lock, setLock] = useState(false)
 
     const boardInputRef = useRef()
 
-    useEffect(() => {
-        if (addingBoard) { setLock(false) }
-    }, [addingBoard])
+    useEffect(() => { boardInputRef.current.focus() }, [])
 
-    const handleWrite = (ev) => {
-        setBoard(ev.target.value)
-    }
+    const handleWrite = (ev) => { setBoard(ev.target.value) }
 
-    const handleAddingBoard = () => {
-        setAddingBoard(true)
-        setTimeout(() => {
-            boardInputRef.current.focus()
-        }, .0001)
+    // const handleAddingBoard = () => {
+    //     setAddingBoard(true)
+    //     setTimeout(() => {
+    //         boardInputRef.current.focus()
+    //     }, .0001)
 
-    }
+    // }
 
-    const handleAddBoard = (ev) => {
+    // const handleAddBoard = (ev) => {
 
-        const newBoard = {
-            type: 'board',
-            title: board,
-            tasks: []
-        }
-        pushBoard(newBoard)
-        setBoard('')
-        setAddingBoard(false)
+    //     const newBoard = {
+    //         type: 'board',
+    //         title: board,
+    //         tasks: []
+    //     }
+    //     pushBoard(newBoard)
+    //     setBoard('')
+    //     setAddingBoard(false)
 
-    }
+    // }
 
     const handleBlur = () => {
-        if (!lock) {
-            setAddingBoard(false)
-        }
+        if (!lock) { unMountOnBlur() }
     }
 
     const lockUnlock = () => {
@@ -53,33 +47,23 @@ function BoardInput({ pushBoard }) {
 
 return (
 <>
-{    
-    !addingBoard
-    ?
-    (<div className='dashboard_notAddingBoard'>
-        <div 
-        className='notAddingBoard_btn'
-        onClick={handleAddingBoard}>+Add Board</div>
-    </div>)
-    :
-    (<div 
+    <div 
     className='boardInput_addingBoard'
     onMouseDown={(ev) => ev.preventDefault()}
-    onClick={lockUnlock}
     onBlur={handleBlur}
+    onClick={lockUnlock}
     >
         <input
         className='addingBoard_input'
-        value={board}
         onChange={handleWrite}
+        value={board}
         ref={boardInputRef}
         />
         <div
         className='addingBoard_saveBtn'
-        onClick={handleAddBoard}
-        >+</div>
-    </div>)
-}
+        onClick={() => handleSave(board)}>+ Save Board</div>
+    </div>
+
 </>
 )
 }

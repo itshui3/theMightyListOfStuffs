a351224a02bd91e9b67981e54f773bd412e1b3f6
Hui <xuhuizhu369@gmail.com>
2021-01-23 21:55:15 -0800

    fixes query syntax for updating board

--- src/components/Board/_updateBoardMutation.js
+++ src/components/Board/_updateBoardMutation.js
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client'
 
 const updateBoardMutation = gql`
-mutation UpdateBoard($username: String!, $pgId: String, $boardId: String!, $title: String!, $tasks: String!){
-    updateBoard(username: $username, pgId: $pgId, boardId: $boardId, title, $title, tasks, $tasks) {
+mutation UpdateBoard($username: String!, $pgId: String!, $boardId: String!, $title: String!, $tasks: String!){
+    updateBoard(username: $username, pgId: $pgId, boardId: $boardId, title: $title, tasks: $tasks) {
         id,
         title,
         tasks
